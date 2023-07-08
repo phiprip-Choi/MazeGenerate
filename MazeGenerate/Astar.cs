f0303a46408ddb8247befff011cc6d67111728ca
phiprip-Choi <132041240+phiprip-Choi@users.noreply.github.com>
2023-07-08 17:21:24 +0900

    코드 간소화

--- MazeGenerate/Astar.cs
+++ MazeGenerate/Astar.cs
@@ -15,12 +15,10 @@ namespace MazeGenerate
         Stage[,] map;
         private readonly int startX, startY, goalX, goalY;
         NodePosition node = new NodePosition(), lastNode = new NodePosition();
-
-        List<NodePosition> nodePath = new List<NodePosition>();
-        HashSet<NodePosition> closeList = new HashSet<NodePosition>();
-        Dictionary<NodePosition, int> openList = new Dictionary<NodePosition, int>();
-        Dictionary<NodePosition, NodePosition> root = new Dictionary<NodePosition, NodePosition>();
-
+        List<NodePosition> nodePath = new List<NodePosition>(); // 궤적 저장용
+        HashSet<NodePosition> closeList = new HashSet<NodePosition>(); // 닫힌 구역(할당된 구역)
+        Dictionary<NodePosition, int> openList = new Dictionary<NodePosition, int>(); // 열린 구역(비할당된 구역)
+        Dictionary<NodePosition, NodePosition> root = new Dictionary<NodePosition, NodePosition>(); // 궤적 기록
 
         public Astar(Stage[,] map)
         {
@@ -28,7 +26,7 @@ namespace MazeGenerate
 
             for (int y = 0; y < map.GetLength(1); y++)
             {
-                for (int x = 0; x < map.GetLength(0); x ++)
+                for (int x = 0; x < map.GetLength(0); x++)
                 {
                     if (Stage.Start == map[x, y])
                     {
@@ -60,7 +58,7 @@ namespace MazeGenerate
             node.Censor(startX, startY);
             openList.Add(node, 0);
             bool isGoal = false;
-            while (openList.Count > 0  && !isGoal)
+            while (openList.Count > 0 && !isGoal)
             {
                 node = openList.OrderBy(k => k.Value).FirstOrDefault().Key; //오름차순으로 정렬해 가장 적은 값(F-cost)을 추출
 
@@ -70,32 +68,31 @@ namespace MazeGenerate
                     {
                         NodePosition currentNode = new NodePosition();
                         currentNode.Censor(xSel, ySel);
+
                         if ((node.x == xSel && node.y == ySel) || //현재 위치한 구역 제외
                             ySel < 0 || ySel > map.GetLength(1) - 2 || // 범위 외 제외
                             xSel < 0 || xSel > map.GetLength(0) - 3 || // 상동
                             closeList.Contains(currentNode) || // 이미 등록된 구역 제외
                             map[xSel, ySel] == Stage.Wall || //벽이 위치한 구역 제외
                             (ySel == node.y + 1 && xSel > node.x) || // 검사 구역 제한
-                            (ySel == node.y + 1 && xSel < node.x)) continue; // 상동
-  
-                            if (openList.ContainsKey(currentNode) && openList[currentNode] < FCost(node, currentNode)) 
-                                continue;
-                            
-                            if((map[xSel, ySel] != Stage.Goal))
-                            {
-                                openList[currentNode] = FCost(node, currentNode);
-                                root[currentNode] = node;
-                            }
-                            else 
-                            {
-                                openList[currentNode] = FCost(node, currentNode);
-                                root[currentNode] = node;
-                                lastNode = currentNode;
-                                isGoal = true;
-                                ySel = node.y + 4;
-                                break;
-                            }
-                        
+                            (ySel == node.y + 1 && xSel < node.x)|| // 상동
+                            (openList.ContainsKey(currentNode) && // 기존 F-Cost보다 높은 경우 제외
+                            openList[currentNode] < FCost(node, currentNode))) continue;
+
+                        if ((map[xSel, ySel] != Stage.Goal))
+                        {
+                            openList[currentNode] = FCost(node, currentNode);
+                            root[currentNode] = node;
+                        }
+                        else
+                        {
+                            openList[currentNode] = FCost(node, currentNode);
+                            root[currentNode] = node;
+                            lastNode = currentNode;
+                            isGoal = true;
+                            ySel = node.y + 4;
+                            break;
+                        }
                     }
                 }
                 closeList.Add(node);
@@ -105,13 +102,13 @@ namespace MazeGenerate
 
         private int FCost(NodePosition prev, NodePosition current)
         {
-            if(prev.x == startX && prev.y == startY) // 초기 구역만 예외로 설정
+            if (prev.x == startX && prev.y == startY) // 초기 구역만 예외로 설정
                 return (int)(Math.Pow(startX - current.x, 2) + Math.Pow(startY - current.y, 2) +
                     Math.Pow(current.x - goalX, 2) + Math.Pow(current.y - goalY, 2));
             else // 나머지 경우. 각각 인접한 구역 간 거리와 이전 구역의 'F-Cost - H-Cost' 값을 서로 더해, 현 구역의 H-Cost 값과 합친다.
                 return (int)(Math.Pow(current.x - prev.x, 2) + Math.Pow(current.y - prev.y, 2) +
                     Math.Pow(current.x - goalX, 2) + Math.Pow(current.y - goalY, 2) +
-                    openList[prev] - Math.Pow(prev.x - goalX, 2) - Math.Pow(prev.y- goalY, 2));
+                    openList[prev] - Math.Pow(prev.x - goalX, 2) - Math.Pow(prev.y - goalY, 2));
         }
 
         public void Tracking(Player p)
